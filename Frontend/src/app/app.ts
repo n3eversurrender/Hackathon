922e6341b5f4b91dbbacc5c96fc353337d4090f5
Zaini <zainiridha8@Gmail.com>
2025-10-02 15:16:04 +0700

    feat: setup app routing, integrate toast component, and add Tailwind CSS directives

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastComponent } from './components/toast.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html'
+  imports: [RouterOutlet, ToastComponent],
+  templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('Frontend');
